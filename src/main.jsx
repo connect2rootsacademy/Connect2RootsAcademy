7e0a03e9fac10e850d48e09619e666f8ecfcb754
Ayush Raj Chauhan <113330291+ayusshhhraj0506@users.noreply.github.com>
2025-11-12 17:05:47 +0530

    gh pages fixed changes 404

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import AuthProvider from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Connect2RootsAcademy">
       <AuthProvider>
         <App />
       </AuthProvider>
