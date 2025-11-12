5e9a3af841625afcc4a51e71710bb51ab89e1797
Ayush Raj Chauhan <113330291+ayusshhhraj0506@users.noreply.github.com>
2025-11-12 20:06:24 +0530

    hash router implemented

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+
 // import { AuthProvider } from './context/AuthContext.jsx';
 import AuthProvider from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
